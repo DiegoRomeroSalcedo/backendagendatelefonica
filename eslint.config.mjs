f1c4af30f0034216872080fc280e89cd5cdf61ec
DiegoRomeroSalcedo <diegosalcedoromero835@gmail.com>
2024-10-30 10:21:14 -0500

    Se configura el linter de slint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,23 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: globals.node } }, // Cambia browser por node
-  pluginJs.configs.recommended,
-  {
+  {// Ignorar la carpeta dist
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "module",
+      globals: globals.node,
+    },
     rules: {
-      semi: ["error", "always"], // Exigir punto y coma
+      "eqeqeq": "error",
+      "no-trailing-spaces": "error",
+      "object-curly-spacing": ["error", "always"],
+      "arrow-spacing": ["error", { "before": true, "after": true }],
+      "no-console": 0,
+      "semi": ["error", "always"], // Asegúrate de que esta regla esté aquí
     },
   },
+  pluginJs.configs.recommended,
+  {
+    ignores: ["dist/**", "build/**"],
+  }
 ];
--- index.js
+++ index.js
@@ -13,26 +13,26 @@ app.use(express.static('dist')); // Con esta linea hacemos que express pueda ser
 const Phone = require('./models/phone');
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
 ];
 
 app.get('/info', (request, response) => {
@@ -53,14 +53,14 @@ app.get('/api/persons', (request, response) => {
 
 app.get('/api/persons/:id', (request, response, next) => {
   Phone.findById(request.params.id)
-  .then(phone => {
-    if (phone) {
-      response.json(phone);
-    } else {
-      response.status(404).end();
-    }
-  })
-  .catch(error => next(error));
+    .then(phone => {
+      if (phone) {
+        response.json(phone);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error));
 });
 
 app.post('/api/persons', (request, response, next) => {
@@ -72,42 +72,42 @@ app.post('/api/persons', (request, response, next) => {
   });
 
   phone.save()
-  .then(savedPhone => {
-    response.json(savedPhone);
-  })
-  .catch(error => next(error));
+    .then(savedPhone => {
+      response.json(savedPhone);
+    })
+    .catch(error => next(error));
 
 });
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Phone.findByIdAndDelete(request.params.id)
-  .then(result => {
-    response.status(204).end();
-  })
-  .catch(error => next(error));
+    .then(result => {
+      response.status(204).end();
+    })
+    .catch(error => next(error));
 });
 
 app.put('/api/persons/:id', (request, response, next) => {
   const { name, number } = request.body;
 
   Phone.findByIdAndUpdate(
-      request.params.id,
-      { name, number },
-      {new: true, runValidators: true, context: 'query'}
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
   )
-  .then(updatePhone => {
-    response.json(updatePhone);
-  })
-  .catch(error => next(error));
+    .then(updatePhone => {
+      response.json(updatePhone);
+    })
+    .catch(error => next(error));
 });
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message);
 
   if (error.name === 'CastError') {
-    return response.status(400).send({Error: 'malformatted id'});
+    return response.status(400).send({ Error: 'malformatted id' });
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({error: error.message});
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
